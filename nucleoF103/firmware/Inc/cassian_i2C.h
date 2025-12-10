4bc8ae7b81f3111b966721f15fe4eed06a4edeca
EliseWlt <elise.weltzer@orange.fr>
2025-12-10 16:20:06 +0100

    ajout i2c

---
+++ nucleoF103/firmware/Inc/cassian_i2C.h
@@ -0,0 +1,21 @@
+/**
+ ******************************************************************************
+ * @file           : cassian_i2c.h
+ * @brief          : Cassian team I2C bus managing team 
+ * @authors        : Cassain team (Louis-Gabriel Brossier, Thomas Chourret, Fatima El-Hajji, Iyed Hamrouni, Victor Loux, Triet Nguyen, Léa Pronzola, Elise Weltzer)
+ ******************************************************************************
+ */ 
+
+#ifndef CASSIAN_I2C_H
+#define CASSIAN_I2C_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "stm32f1xx_hal_i2c.h"
+
+#ifdef __cplusplus
+}
+#endif
+#endif //CASSIAN_I2C_H
